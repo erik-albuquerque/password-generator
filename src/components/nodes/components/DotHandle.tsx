c43bd5f41a14506e7155cf9ee6ed2b791e053700
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-04-13 19:37:46 -0300

    fix: all position

--- src/components/nodes/components/DotHandle.tsx
+++ src/components/nodes/components/DotHandle.tsx
@@ -11,7 +11,13 @@ const DotHandle = ({ position, type, ...rest }: DotHandleProps) => {
       id={position}
       type={type}
       position={position}
-      className={clsx('w-2 h-2 bg-gray-400 border-none', `-${position}-2`)}
+      className={clsx(
+        'w-2 h-2 bg-gray-400 border-none',
+        position === 'top' && '-top-2',
+        position === 'left' && '-left-2',
+        position === 'right' && '-right-2',
+        position === 'bottom' && '-bottom-2'
+      )}
       {...rest}
     />
   )
