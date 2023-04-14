4f397fcdc0a9a6c5504f05a85a64a82c82e902d0
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-04-14 15:32:55 -0300

    fix: move recoil root to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,30 @@
 import { ConnectionMode } from 'reactflow'
-import { RecoilRoot } from 'recoil'
 import { ReactFlow } from './components'
-import { NODE_TYPES, EDGE_TYPES } from './constants'
 import { useEdges, useNodes } from './hooks'
+import { useFlow } from './hooks/useFlow'
 
 const App = () => {
+  const { NODE_TYPES, EDGE_TYPES } = useFlow()
+
   const { edges, onEdgesChange, onConnect } = useEdges()
   const { nodes, onNodesChange } = useNodes()
 
   return (
-    <RecoilRoot>
-      <ReactFlow.Root
-        nodeTypes={NODE_TYPES}
-        edgeTypes={EDGE_TYPES}
-        nodes={nodes}
-        edges={edges}
-        onConnect={onConnect}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        connectionMode={ConnectionMode.Loose}
-        defaultEdgeOptions={{
-          type: 'default'
-        }}
-      >
-        <ReactFlow.Controls />
-      </ReactFlow.Root>
-    </RecoilRoot>
+    <ReactFlow.Root
+      nodeTypes={NODE_TYPES}
+      edgeTypes={EDGE_TYPES}
+      edges={edges}
+      nodes={nodes}
+      onConnect={onConnect}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      connectionMode={ConnectionMode.Loose}
+      defaultEdgeOptions={{
+        type: 'default'
+      }}
+    >
+      <ReactFlow.Controls />
+    </ReactFlow.Root>
   )
 }
 
