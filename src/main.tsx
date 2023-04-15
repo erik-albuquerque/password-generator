fce1aeee8a380e8bc2d6ccd78f4e613bc1021f61
Érik Albuquerque <erik.albuquerque.oficial@gmail.com>
2023-04-15 16:35:02 -0300

    fix: import global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import './styles/main.css'
-
+/* eslint-disable simple-import-sort/imports */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RecoilRoot } from 'recoil'
 
 import { App } from './App'
+
+import './styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
